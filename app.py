44fbdb534562904de5846fbb7a62f775d45815c4
inovux <stanley.thijssen@gmail.com>
2020-06-01 22:02:51 +0800

    feat: Added create artist functionality

--- app.py
+++ app.py
@@ -431,12 +431,30 @@ def create_artist_submission():
   # called upon submitting the new artist listing form
   # TODO: insert form data as a new Venue record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
+  error = False
+
+  try:
+    artist = Artist(name = request.form['name'], city = request.form['city'], state = request.form['state'], phone = request.form['phone'], facebook_link = request.form['facebook_link'])
+    db.session.add(artist)
+    db.session.commit()
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+
+  if error:
+    flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+  else:
+    flash('Artist ' + request.form['name'] + ' was successfully listed!')
+    return render_template('pages/home.html') 
 
   # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
+  
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
-  return render_template('pages/home.html')
+  
 
 
 #  Shows
